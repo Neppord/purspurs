1b765b85fe404073bc877e5d93ef549d36d8380e
Samuel Ytterbrink <neppord@gmail.com>
2024-04-01 21:33:14 +0200

    Support value declarations in the github pages

--- src/Interpreter.purs
+++ src/Interpreter.purs
@@ -3,19 +3,22 @@ module Interpreter where
 import Prelude
 
 import Data.Map.Internal (Map)
-import Data.Maybe (Maybe(..))
-import Parser (Expr(..), Value(..), parse_expression)
+import Data.Maybe (Maybe(..), maybe)
+import Parser (Declaration(..), Expr(..), Value(..), parse_declaration, parse_expression)
 import Data.Map.Internal (empty, insert, lookup) as Map
+import Data.Ord (lessThan)
 
 type Env = Map String Value
 
 evaluate_expr :: Env -> Expr -> Value
 evaluate_expr _ (ExprValue value) = value
-evaluate_expr env (ExprApp f a) = let
+evaluate_expr env (ExprApp f a) =
+  let
     argument = evaluate_expr env a
-    in case evaluate_expr env f of
-        ValueLambda key expr -> evaluate_expr (env # Map.insert key argument) expr
-        _ -> ValueError
+  in
+    case evaluate_expr env f of
+      ValueLambda key expr -> evaluate_expr (env # Map.insert key argument) expr
+      _ -> ValueError
 evaluate_expr env (ExprIdentifier key) = case Map.lookup key env of
   Just v -> v
   Nothing -> ValueError
@@ -25,5 +28,17 @@ evaluate_expr _ _ = ValueError
 interpret_expr :: String -> Value
 interpret_expr expr = evaluate_expr Map.empty (parse_expression expr)
 
-print :: Value -> String
-print v = show v
+evaluate :: String -> Env -> Env
+evaluate s env = case parse_declaration s of
+  DeclarationValue name expr -> let
+    value = evaluate_expr env expr
+    in env
+        # Map.insert name value
+        # Map.insert "_" value
+  DeclarationError -> env # Map.insert "_" (evaluate_expr env (parse_expression s))
+
+print :: Env -> String
+print env = Map.lookup "_" env # maybe "<Error>" show
+
+default_env :: forall k v. Map k v
+default_env = Map.empty
